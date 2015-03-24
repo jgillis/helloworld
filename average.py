99eef4dbbb56a2701d387e9d2713434fdedb1a5a
jgillis <joris.gillis42@gmail.com>
2015-03-24 10:54:23 +0100

    Adding doctests

--- average.py
+++ average.py
@@ -3,6 +3,19 @@ class Averager:
 
     This class computes the average of all the samples that you add to it.
 
+	Examples:
+	  >> avg = Averager()
+	  >> avg.add(12)
+	  >> avg.add(14)
+	  >> print avg()
+	  13
+	  
+	  >> avg = Averager()
+	  >> avg.add(2)
+	  >> avg.add(3)
+	  >> print avg()
+	  2.5
+	  
     """
     def __init__(self):
         """
