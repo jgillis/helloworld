68da361bfa95fe43fc0984c344c74ff9d72f1bc6
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-03-24 11:15:57 +0100

    Corecting unittests

--- average.py
+++ average.py
@@ -4,16 +4,16 @@ class Averager:
     This class computes the average of all the samples that you add to it.
 
 	Examples:
-	  >> avg = Averager()
-	  >> avg.add(12)
-	  >> avg.add(14)
-	  >> print avg()
-	  13
+	  >>> avg = Averager()
+	  >>> avg.add(12)
+	  >>> avg.add(14)
+	  >>> print avg()
+	  13.0
 	  
-	  >> avg = Averager()
-	  >> avg.add(2)
-	  >> avg.add(3)
-	  >> print avg()
+	  >>> avg = Averager()
+	  >>> avg.add(2)
+	  >>> avg.add(3)
+	  >>> print avg()
 	  2.5
 	  
     """
